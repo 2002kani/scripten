ab6ac6d6413ac67c31f8c4a1de3dc6b070b9455b
02Kani <kanikaradag02@gmail.com>
2025-05-06 12:59:21 +0200

    #

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,10 +1,10 @@
 import { app, BrowserWindow } from "electron"
 import path from "path"
-import { isDev } from "./utils.js";
+import { isDev } from "./utils.js"; 
 
-app.on("ready", () => { 
+app.on("ready", () => {  
 	const mainWindow = new BrowserWindow({});
-	if(isDev()){
+	if(isDev()){ 
 		mainWindow.loadURL("http://localhost:5123");
 	}else{ 
 		mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
