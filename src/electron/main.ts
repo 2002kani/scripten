6f081481d110190edcb789ceabebab180b7f22ed
02Kani <kanikaradag02@gmail.com>
2025-05-06 12:59:08 +0200

    #

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -6,7 +6,7 @@ app.on("ready", () => {
 	const mainWindow = new BrowserWindow({});
 	if(isDev()){
 		mainWindow.loadURL("http://localhost:5123");
-	}else{
+	}else{ 
 		mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
 	}
 });
